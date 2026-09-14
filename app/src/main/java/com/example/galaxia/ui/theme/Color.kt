c1984f18bce851aeead545388201ae5cf665894e
GustavoACVieira <gustavoadcv@gmail.com>
2026-09-14 17:56:33 -0300

    SCRUM-6: Tela Splash

--- app/src/main/java/com/example/galaxia/ui/theme/Color.kt
+++ app/src/main/java/com/example/galaxia/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Paleta GalaxIA
+val Cyan70 = Color(0xFF00B8E6)
+val Grey95 = Color(0xFFF5F5F5)
+val Grey60 = Color(0xFF9A9AA2)
+val Grey10 = Color(0xFF0D0E13)
+val Grey0 = Color(0xFF050609)
